eb961acc757216d0d1656700820fcfc53c7ac8b4
Alex Johnson <112223622+AeJohnson13@users.noreply.github.com>
2025-11-03 17:11:27 -0900

    worked

--- bridges.cpp
+++ bridges.cpp
@@ -12,73 +12,53 @@
 #include <vector>
 using std::vector;
 using std::size_t;
+#include <algorithm>
+using std::sort;
+
+//for debug 
 #include <iostream>
 using std::cout;
-using std::endl;
-bool checkset(vector<Bridge> &set,
-			  Bridge curr)
-{
-// need to do some table shit here
-	for( auto bridge : set){
-		if(bridge[1] == curr[1])
-			return false;
-		if(bridge[0] == curr[0])
-			return false;
-  	if (bridge[0] < curr[0] && bridge[1] > curr[1])
- 			return false;
-		if (bridge[0] > curr[0] && bridge[1] < curr[1])
-  		return false;
-	}
-	return true;
-}
 
-int sum_of(vector<Bridge>& set)
+
+bool checkpair(const Bridge& a,
+			   const Bridge& b)
 {
-	if(set.size() == 0)
-		return 0;
-	int sum = 0;	
-	for(auto i : set){
-		sum+=i[2];
-	}
-	return sum;
+	if (a[1] == b[1])
+        return a[0] < b[0];
+    return a[1] < b[1];
 }
 	
 
-void bridgeRecur(size_t i, 
-				const vector<Bridge>& bridges, 							
-				int& maxes, 
-				vector<Bridge>& subset) 
-{
-  // base case
-	// checks for a higher max and updates if one is found
-  if (i == bridges.size()) {
-  int sum = sum_of(subset);
-    if(sum > maxes)
-		  maxes = sum;
-		return;   
-    }
-    
-		// check if the current bridge can be added,
-		// if it can, add to subset and recurse
-		if(checkset(subset, bridges[i])){
-    	subset.push_back(bridges[i]);
-    	bridgeRecur(i+1, bridges, maxes, subset);
-		subset.pop_back();
-    }
-    // recurse without including the current bridge 
-    bridgeRecur(i+1, bridges, maxes, subset);
-}
-
-
-
-
 int bridges(int w, 
-			      int e,
-			      const vector<Bridge> & bridges){
+			int e,
+			const vector<Bridge>& bridges){
 
-    int max = 0;
+	const size_t n = bridges.size();
+   
+	vector<Bridge> sorted = bridges;
 
-	vector<Bridge> subsets = {};
-	bridgeRecur(size_t(0), bridges, max, subsets);
-	return max;
+
+
+    sort(sorted.begin(), sorted.end(), checkpair);
+
+	vector<int> maxes(n);		
+    for(size_t i = 0; i < n; i++)
+    	maxes[i] = sorted[i][2];
+	
+    for(size_t i=0; i<n; i++)
+	{
+        for(size_t j=0; j<i; j++){
+            if (sorted[i][0] > sorted[j][0] && sorted[i][1] > sorted[j][1])
+                 maxes[i] = std::max(maxes[i], maxes[j] + sorted[i][2]);
+		}
+	}  
+
+    int maxtoll = 0;
+    for(size_t i=0; i<n; i++){
+        if (maxtoll < maxes[i])
+            maxtoll = maxes[i];
+	}
+    return maxtoll;        
 }
+
+
--- gfg_bridges.cpp
+++ gfg_bridges.cpp
@@ -1,59 +1,45 @@
-// C++ implementation of building bridges
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-// north-south coordinates
-// of each City Pair
-struct CityPairs
-{
+// north-south coordinates of each City Pair
+struct CityPairs {
     int north, south;
 };
 
-// comparison function to sort
-// the given set of CityPairs
-bool compare(struct CityPairs a, struct CityPairs b)
-{
+// comparison function to sort the given set of CityPairs
+bool compare(const CityPairs& a, const CityPairs& b) {
     if (a.south == b.south)
         return a.north < b.north;
     return a.south < b.south;
 }
 
-// function to find the maximum number
-// of bridges that can be built
-int maxBridges(struct CityPairs values[], int n)
-{
-    int lis[n];
-    for (int i=0; i<n; i++)
-        lis[i] = 1;
-        
-    sort(values, values+n, compare);
-    
-    // logic of longest increasing subsequence
-    // applied on the northern coordinates
-    for (int i=1; i<n; i++)
-        for (int j=0; j<i; j++)
-            if (values[i].north >= values[j].north 
-                && lis[i] < 1 + lis[j])
+// function to find the maximum number of bridges that can be built
+int maxBridges(CityPairs values[], int n) {
+    vector<int> lis(n, 1);
+
+    sort(values, values + n, compare);
+
+    // logic of longest increasing subsequence on northern coordinates
+    for (int i = 1; i < n; i++)
+        for (int j = 0; j < i; j++)
+            if (values[i].north > values[j].north && lis[i] < 1 + lis[j])
                 lis[i] = 1 + lis[j];
-        
-        
-    int max = lis[0];
-    for (int i=1; i<n; i++)
-        if (max < lis[i])
-            max = lis[i];
-    
-    // required number of bridges
-    // that can be built        
-    return max;        
+
+    int maxcount = lis[0];
+    for (int i = 1; i < n; i++){
+        cout << lis[i] << "\n";
+        if (maxcount < lis[i])
+            maxcount = lis[i];
+    }
+    return maxcount;
 }
 
 // Driver program to test above
-int main()
-{
-    struct CityPairs values[] = {{6, 2}, {4, 3}, {2, 6}, {1, 5}};
+int main() {
+    CityPairs values[] = {{6, 2}, {4, 3}, {2, 6}, {1, 5}};
     int n = 4;
-    cout << "Maximum number of bridges = " 
-             << maxBridges(values, n);    
+    cout << "Maximum number of bridges = " << maxBridges(values, n) << endl;
     return 0;
 }
